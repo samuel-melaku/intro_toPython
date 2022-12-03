0d819556fe14544a67d41616d1dbb99af85f76f0
Samuel Melaku <samuel.melaku@hgs.hiddengeniusproject.org>
2022-12-03 11:54:24 -0800

    branch

---
+++ exercise4.py
@@ -0,0 +1,16 @@
+a = 3
+b = 5
+c = 0 # placeholder
+print("a: " + str(a))
+
+print("b: " + str(b))
+
+
+print("\n")
+
+c = a # c = 3
+a = b # a = 5
+b = c # b = 3
+print ("a: " + str(a))
+
+print("b: " + str(b))
